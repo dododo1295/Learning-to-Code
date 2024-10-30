7c7d2dcef49409a9f8ddca50ca6d1a986aa6c285
Dallas S. <andrewdallassherman@gmail.com>
2024-10-30 20:42:59 +0900

    finished the first lesson

--- Javascript Beta Course/pyramidGenerator.js
+++ Javascript Beta Course/pyramidGenerator.js
@@ -1,13 +1,19 @@
 const character = "#";
-const count = 8;
+const count = 10;
 const rows = [];
+// This is for inversion; true inverts.
+let inverted = false;
 
 function padRow(rowNumber, rowCount) {
-  return " " + character.repeat(rowNumber) + " ";
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(padRow(i + 1, count));
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
 }
 
 let result = ""

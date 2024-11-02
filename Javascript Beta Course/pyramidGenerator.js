3aa571141eaf7a16348c9c818d7ffff7e97038cd
Dallas S. <andrewdallassherman@gmail.com>
2024-11-02 11:43:32 +0900

    here we go

--- Javascript Beta Course/pyramidGenerator.js
+++ Javascript Beta Course/pyramidGenerator.js
@@ -5,7 +5,11 @@ const rows = [];
 let inverted = false;
 
 function padRow(rowNumber, rowCount) {
-  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+  return (
+    " ".repeat(rowCount - rowNumber) +
+    character.repeat(2 * rowNumber - 1) +
+    " ".repeat(rowCount - rowNumber)
+  );
 }
 
 for (let i = 1; i <= count; i++) {
@@ -16,10 +20,11 @@ for (let i = 1; i <= count; i++) {
   }
 }
 
-let result = ""
+let result = "";
 
 for (const row of rows) {
   result = result + "\n" + row;
 }
 
 console.log(result);
+// this is a comment to test a git push...

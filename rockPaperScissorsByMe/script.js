3940487c3b6a093e86bbe35654cb45f834f206e0
Dallas S. <andrewdallassherman@gmail.com>
2024-11-06 13:31:37 +0900

    starting to understand how to do this myself, trying to rebuild a rock paper scissors

--- rockPaperScissorsByMe/script.js
+++ rockPaperScissorsByMe/script.js
@@ -1,9 +1,32 @@
-const userMove;
-const computerMove;
-const rpsResult;
-const coinFlipResult();
+const userMoveElement = document.querySelector('.user-move');
+const rpsResult = document.querySelector('.rps-result');
+const playerScoreElement = document.querySelector('.player-score');
+const computerScoreElement = document.querySelector('.computer-score');
+const userScore = 0;
+const computerScore = 0;
 
-function gameResult();
-function coinFlip(userChoice) {
-  const flipResult
+function computerMoveResult() {
+  const option = ["rock", "paper", "scissors"];
+  const randomNumber = Math.floor(Math.random() * option.length);
+  return option[randomNumber];
+};
+function coinFlip() {
+  const randomNumber = Math.random();
+  const flipResult = randomNumber < 0.5 ? "heads" : "tails";
+  console.log(flipResult());
+  if (flipResult === "heads") {
+    userScore++;
+    playerScoreElement.innerHTML = userScore;
+  } else {
+    computerScore++
+    computerScoreElement.innerHTML = computerScore;
+  };
+};
+function tieResult() { 
+  if (gameResult === "tie") {
+    coinFlip();
+  }
+};
+playGame() {
+
 };

227b535b97aad64995c47b61cc0bfaaf2394ccc9
Dallas S. <andrewdallassherman@gmail.com>
2024-11-09 11:05:32 +0900

    some thing about a { in my html... i don't see it, may remake or depricate and start over.

--- rockPaperScissorsByMe/script.js
+++ rockPaperScissorsByMe/script.js
@@ -1,15 +1,8 @@
-const userMoveElement = document.querySelector('.user-move');
-const rpsResult = document.querySelector('.rps-result');
 const playerScoreElement = document.querySelector('.player-score');
 const computerScoreElement = document.querySelector('.computer-score');
 let userScore = 0;
 let computerScore = 0;
 
-function computerMoveResult() {
-  const option = ["rock", "paper", "scissors"];
-  const randomNumber = Math.floor(Math.random() * option.length);
-  return option[randomNumber];
-};
 function coinFlip() {
   const randomNumber = Math.random();
   const flipResult = randomNumber < 0.5 ? "heads" : "tails";
@@ -23,7 +16,7 @@ function coinFlip() {
   };
 };
 
-function whoWon(player, computer) {
+/* function whoWon(player, computer) {
   if (player === computer) {
     coinFlip();
   } else if {(
@@ -32,50 +25,45 @@ function whoWon(player, computer) {
   (player === "Paper" && computerMoveResult() === "Rock")
 ),
 };
-/* 
+*/ 
 function playGame(playerMove) {
         const computerMove = pickComputerMove();
 
         let result = '';
 
-        if (playerMove === 'scissors') {
-          if (computerMove === 'rock') {
+        if (playerMove === 'Scissors') {
+          if (computerMove === 'Rock') {
             result = 'You lose.';
-          } else if (computerMove === 'paper') {
+          } else if (computerMove === 'Paper') {
             result = 'You win.';
-          } else if (computerMove === 'scissors') {
+          } else if (computerMove === 'Scissors') {
             result = 'Tie.';
           }
 
-        } else if (playerMove === 'paper') {
-          if (computerMove === 'rock') {
+        } else if (playerMove === 'Paper') {
+          if (computerMove === 'Rock') {
             result = 'You win.';
-          } else if (computerMove === 'paper') {
+          } else if (computerMove === 'Paper') {
             result = 'Tie.';
-          } else if (computerMove === 'scissors') {
+          } else if (computerMove === 'Scissors') {
             result = 'You lose.';
           }
           
-        } else if (playerMove === 'rock') {
-          if (computerMove === 'rock') {
+        } else if (playerMove === 'Rock') {
+          if (computerMove === 'Rock') {
             result = 'Tie.';
-          } else if (computerMove === 'paper') {
+          } else if (computerMove === 'Paper') {
             result = 'You lose.';
-          } else if (computerMove === 'scissors') {
+          } else if (computerMove === 'Scissors') {
             result = 'You win.';
           }
         }
 
         if (result === 'You win.') {
-          score.wins += 1;
+          playerScoreElement += 1;
         } else if (result === 'You lose.') {
-          score.losses += 1;
+          computerScoreElement += 1;
         } else if (result === 'Tie.') {
-          score.ties += 1;
+          coinFlip();
         }
-*/ // MAYBE??? i don't think i can do this without a using this if statements, template literals probably can't from my research.
-function playGame("") {
-  computerMoveResult();
-  whoWon();
-
-};
+      }

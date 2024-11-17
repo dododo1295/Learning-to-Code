919305a6ce3ef369664a5011dca4bb256d1ee82b
Dallas S. <andrewdallassherman@gmail.com>
2024-11-17 17:05:57 +0900

    testing deno stuff

--- test.ts
+++ test.ts
@@ -1,6 +1,7 @@
-const data = 1
+let data = 1
 if (data === 1) {
     console.log(data + 1);
   }
 
-const dududu = data === 1 ? console.log(data + 1) : console.log("it didn't work")
+const dududu = data === 1 ? console.log(data + 45) : console.log("it didn't work")
+console.log(dududu);

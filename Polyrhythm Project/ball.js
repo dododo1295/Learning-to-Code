9f1230ed292b68be638bd47d5abce2933912b5a7
Dallas S. <andrewdallassherman@gmail.com>
2024-12-14 12:42:34 +0900

    cool it's starting to work but there's another method to use instead.

--- Polyrhythm Project/ball.js
+++ Polyrhythm Project/ball.js
@@ -1,21 +1,36 @@
 class Ball {
-  constructor(track, radius, speed) {
+  constructor(track, radius, speed, soundFreq) {
     this.track = track;
     this.radius = radius;
     this.speed = speed;
+    this.soundFreq = soundFreq;
     this.offset = 0;
+    this.round = 0;
     this.center = this.track.getPosition(this.offset);
   }
 
   move() {
     this.offset += this.speed;
-    this.center = this.track.getPosition(this.offset);
+    const res = (this.center = this.track.getPosition(this.offset));
+    this.center = { x: res.x, y: res.y };
+    if (res.round != this.round) {
+      this.direction = -1;
+      playSound(this.soundFreq);
+      this.round = res.round;
+    }
   }
   draw(ctx) {
-    ctx.beginPath();
-    ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = "white";
-    ctx.stroke();
+    const fakeY = 2 * this.track.center.y - this.center.y;
+    if (fakeY > this.center.y) {
+      ctx.beginPath();
+      ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
+      ctx.strokeStyle = "white";
+      ctx.stroke();
+    } else {
+      ctx.beginPath();
+      ctx.arc(this.center.x, fakeY, this.radius, 0, Math.PI * 2);
+      ctx.strokeStyle = "white";
+      ctx.stroke();
+    }
   }
 }
--- Polyrhythm Project/track.js
+++ Polyrhythm Project/track.js
@@ -2,16 +2,22 @@ class Track {
   constructor(center, radius) {
     this.center = center;
     this.radius = radius;
+    this.period = Math.PI;
   }
   getPosition(offset) {
     return {
       x: this.center.x + Math.cos(offset) * this.radius,
       y: this.center.y - Math.sin(offset) * this.radius,
+      round: Math.floor(offset / this.period),
     };
   }
   draw(ctx) {
     ctx.beginPath();
-    ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
+    for (let a = 0; a < Math.PI * 2; a += 0.01) {
+      const pos = this.getPosition(a);
+      ctx.lineTo(pos.x, pos.y);
+    }
+    ctx.closePath();
     ctx.strokeStyle = "white";
     ctx.stroke();
   }

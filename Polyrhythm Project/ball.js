09dba8c3f6b57a47a39933cb0d80be09c156bfa7
Dallas S. <andrewdallassherman@gmail.com>
2024-12-14 18:16:10 +0900

    finished this... really cool learning exercise

--- Polyrhythm Project/ball.js
+++ Polyrhythm Project/ball.js
@@ -1,36 +1,35 @@
 class Ball {
-  constructor(track, radius, speed, soundFreq) {
+  constructor(track, radius, speed, soundFrequency, hue) {
     this.track = track;
     this.radius = radius;
     this.speed = speed;
-    this.soundFreq = soundFreq;
+    this.soundFrequency = soundFrequency;
+    this.hue = hue;
     this.offset = 0;
     this.round = 0;
+    this.progress = 0;
     this.center = this.track.getPosition(this.offset);
   }
 
   move() {
     this.offset += this.speed;
-    const res = (this.center = this.track.getPosition(this.offset));
+    const res = this.track.getPosition(this.offset);
     this.center = { x: res.x, y: res.y };
+    this.progress = res.progress;
     if (res.round != this.round) {
-      this.direction = -1;
-      playSound(this.soundFreq);
+      playSound(this.soundFrequency);
       this.round = res.round;
     }
   }
+
   draw(ctx) {
-    const fakeY = 2 * this.track.center.y - this.center.y;
-    if (fakeY > this.center.y) {
-      ctx.beginPath();
-      ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
-      ctx.strokeStyle = "white";
-      ctx.stroke();
-    } else {
-      ctx.beginPath();
-      ctx.arc(this.center.x, fakeY, this.radius, 0, Math.PI * 2);
-      ctx.strokeStyle = "white";
-      ctx.stroke();
-    }
+    ctx.beginPath();
+    ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "white";
+    const lightness = 100 - 50 * this.progress;
+    ctx.fillStyle = `hsl(${this.hue}, 100%, ${lightness}%)`;
+    ctx.fill();
+    ctx.stroke();
   }
 }

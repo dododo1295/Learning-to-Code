5b0b3e81e836e21f7f7b39c21ba53e3060b3d2d3
Dallas S. <andrewdallassherman@gmail.com>
2024-12-21 15:57:53 +0900

    finished first oop

--- js-beta-course/telephone-validator/script.js
+++ js-beta-course/telephone-validator/script.js
@@ -1,16 +1,29 @@
-const usrInput = document.getElementById("user-input");
-const checkBtn = document.getElementById("check-btn");
-const clearBtn = document.getElementById("clear-btn");
+const input = document.getElementById("user-input");
+const check = document.getElementById("check-btn");
+const clear = document.getElementById("clear-btn");
 const results = document.getElementById("results-div");
 
-checkBtn.addEventListener("click", () => {
-  const numIn = usrInput.value.trim();
-  const numArr = numIn.split("");
-  results.innerText = numArr;
-  console.log(numArr);
-
-  if (numArr.length === 0) {
-    results.innerText = "Please provide a phone number";
+check.addEventListener("click", () => {
+  const regex = /^(1\s?)?(\(\d{3}\)|\d{3})([\s-]?)\d{3}([\s-]?)\d{4}$/;
+  if (!input.value) {
+    alert("Please provide a phone number");
+  } else if (regex.test(input.value)) {
+    results.innerText = `Valid US number: ${input.value}`;
+  } else {
+    results.innerText = `Invalid US number: ${input.value}`;
   }
 });
+clear.addEventListener("click", () => {
+  results.innerText = "";
+});
+
+//
+//
 // potential regex for errors? /^[0-9\(\)\-]*$/
+
+//  1 w/ space  (1\s?)?
+//  opt paren  (\(\d{3}\)|\d{3})
+//  opt hyphen ([\s-]?)
+//  3 dig \d{3}
+//  opt hyphen ([\s-]?)
+// 4 dig \d{4}
